4f513cfa7bfa233413aabf3fa0bea35cc269b7dc
Alhassan Joshua Abel <saintjosh26@gmail.com>
2023-04-14 14:29:37 +0000

    The math object

---
+++ 12-math-object/script.js
@@ -0,0 +1,27 @@
+
+// console.log("hello world")
+let x;
+
+x = Math;
+
+// Round up 
+x = Math.ceil(4.2);
+
+// Round down decimal
+x = Math.floor(4.9)
+
+// Raise a number to the power
+x = Math.pow(2,3);
+
+// Find a minimum number
+x = Math.min(4,5,3);
+
+// Find a maximum value
+x = Math.max(4,5,3);
+
+// Generate a random value between 0 - 9
+x = Math.random();
+
+// Generate a random value and round down
+x = Math.floor(Math.random() * 100 )
+console.log(x)
