f83bd7c8a0d9683c5c6f1a8337d7f86d9ed84efc
Alhassan Joshua Abel <41088053+Josh-Alhassan@users.noreply.github.com>
2023-06-15 19:09:54 +0100

    Joke generator script file update

--- 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator/script.js
+++ 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator/script.js
@@ -1,23 +1,36 @@
+// Get the joke element and joke button from the HTML
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('joke-btn');
 
+// Function to generate a Chuck Norris joke
 const generateJoke = () => {
+    // Create a new XMLHttpRequest object
     const xhr = new XMLHttpRequest();
 
+    // Set up the request with the Chuck Norris API
     xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
 
+    // Define the callback function to handle the response
     xhr.onreadystatechange = function () {
+        // Check if the request has been completed
         if (this.readyState === 4) {
+            // Check if the response status is successful
             if (this.status === 200) {
+                // Parse the response as JSON and update the joke element with the joke value
                 jokeEl.innerHTML = JSON.parse(this.responseText).value;
             } else {
-                jokeEl.innerHTML = 'Something went wrong (Not Funny';
+                // Display an error message if the request was not successful
+                jokeEl.innerHTML = 'Something went wrong (Not Funny)';
             }
         }
     };
 
-    xhr.send()
+    // Send the request to retrieve a Chuck Norris joke
+    xhr.send();
 };
 
+// Add an event listener to the joke button to generate a joke when clicked
 jokeBtn.addEventListener('click', generateJoke);
+
+// Generate a joke when the DOM content has finished loading
 document.addEventListener('DOMContentLoaded', generateJoke);
